1658a83eb6bc867f4fe94b2bc8272bffc6a31220
Danya Minakov <dany.minakov@gmail.com>
2023-09-22 20:15:34 +0300

    Fix instruction opcode representation from signed to unsigned

--- src/main/java/org/bytecodeparser/instruction/InstructionReader.java
+++ src/main/java/org/bytecodeparser/instruction/InstructionReader.java
@@ -11,29 +11,31 @@ import java.util.Map;
 
 public class InstructionReader {
 
-  private static final Map<Byte, Class<? extends Instruction>> opCodeToInstruction;
+  private static final Map<Integer, Class<? extends Instruction>> opCodeToInstruction;
 
   static {
     Reflections reflections = new Reflections("org.bytecodeparser.instruction");
     opCodeToInstruction = StreamEx.of(reflections.getSubTypesOf(Instruction.class))
-        .toMap(inst -> (byte) inst.getAnnotation(Opcode.class).value(), i -> i);
+        .toMap(inst -> inst.getAnnotation(Opcode.class).value(), i -> i);
   }
 
   public static List<Instruction> read(byte[] data) {
     DataInputStream inputStream = new DataInputStream(new ByteArrayInputStream(data));
     List<Instruction> instructions = new ArrayList<>();
     byte opcode = -1;
+    int uOpcode = opcode;
     try {
       while (inputStream.available() > 0) {
         if ((opcode = inputStream.readByte()) == 0) {
           break;
         }
+        uOpcode = Byte.toUnsignedInt(opcode);
         Instruction instruction =
-                opCodeToInstruction.get(opcode).getConstructor(DataInputStream.class).newInstance(inputStream);
+                opCodeToInstruction.get(uOpcode).getConstructor(DataInputStream.class).newInstance(inputStream);
         instructions.add(instruction);
       }
     } catch (Exception e) {
-      throw new RuntimeException("Unable tor read instruction with opcode = " + opcode + "\n" + e);
+      throw new RuntimeException("Unable tor read instruction with opcode = " + uOpcode + "\n" + e);
     }
     return instructions;
   }
