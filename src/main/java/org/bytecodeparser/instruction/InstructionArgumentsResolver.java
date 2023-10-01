8fa52238e05bea34dbc645ba50dccdecf1c14f9b
shvetsov <dev.anton.shvetsov@gmail.com>
2023-10-01 10:30:11 +0300

    Generify throwableWrapper

--- src/main/java/org/bytecodeparser/instruction/InstructionArgumentsResolver.java
+++ src/main/java/org/bytecodeparser/instruction/InstructionArgumentsResolver.java
@@ -3,28 +3,28 @@ package org.bytecodeparser.instruction;
 import java.io.DataInputStream;
 import java.util.function.Function;
 
-import static org.bytecodeparser.utility.ThrowingFunction.resolveStrategyWrapper;
+import static org.bytecodeparser.utility.ThrowingFunction.throwableWrapper;
 
 public enum InstructionArgumentsResolver {
     EMPTY(dataInputStream -> InstructionArguments.EMPTY, arguments -> ""),
-    SIMPLE_BYTE(resolveStrategyWrapper(
+    SIMPLE_BYTE(throwableWrapper(
             dataInputStream -> InstructionArguments.builder()
                     .indexByte(dataInputStream.readByte())
                     .build()),
             arguments -> " " + arguments.getIndexByte()),
-    SIMPLE_SHORT(resolveStrategyWrapper(
+    SIMPLE_SHORT(throwableWrapper(
             dataInputStream -> InstructionArguments.builder()
                     .indexShort((short) ((dataInputStream.readByte() << 8) | dataInputStream.readByte()))
                     .build()),
             arguments -> " " + arguments.getIndexShort()),
-    TRIPLE_SHORT_BYTE_ZERO(resolveStrategyWrapper(
+    TRIPLE_SHORT_BYTE_ZERO(throwableWrapper(
             dataInputStream -> InstructionArguments.builder()
             .indexShort((short) ((dataInputStream.readByte() << 8) | dataInputStream.readByte()))
             .count(dataInputStream.readByte())
             .zero1(dataInputStream.readByte())
             .build()),
             arguments -> " " + arguments.getIndexShort() + " " + arguments.getCount() + " " + arguments.getZero1()),
-    TRIPLE_SHORT_ZERO_ZERO(resolveStrategyWrapper(dataInputStream -> InstructionArguments.builder()
+    TRIPLE_SHORT_ZERO_ZERO(throwableWrapper(dataInputStream -> InstructionArguments.builder()
             .indexShort((short) ((dataInputStream.readByte() << 8) | dataInputStream.readByte()))
             .zero1(dataInputStream.readByte())
             .zero2(dataInputStream.readByte())
--- src/main/java/org/bytecodeparser/utility/ThrowingFunction.java
+++ src/main/java/org/bytecodeparser/utility/ThrowingFunction.java
@@ -1,20 +1,16 @@
 package org.bytecodeparser.utility;
 
-import org.bytecodeparser.instruction.InstructionArguments;
-
-import java.io.DataInputStream;
-import java.io.IOException;
 import java.util.function.Function;
 
 @FunctionalInterface
 public interface ThrowingFunction<T, E extends Exception, V> {
     V apply(T t) throws E;
 
-    static Function<DataInputStream, InstructionArguments> resolveStrategyWrapper(ThrowingFunction<DataInputStream, IOException, InstructionArguments> function) {
-        return dataInputStream -> {
+    static <T, E extends Exception, V> Function<T, V> throwableWrapper(ThrowingFunction<T, E, V> function) {
+        return t -> {
             try {
-                return function.apply(dataInputStream);
-            } catch (IOException ex) {
+                return function.apply(t);
+            } catch (Exception ex) {
                 throw new RuntimeException(ex);
             }
         };
