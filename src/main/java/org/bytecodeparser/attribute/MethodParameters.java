4f9f98479f1b1b85474cfc428a824de924b3e138
Daniil Minakov <dany.minakov@gmail.com>
2024-01-05 16:59:57 +0200

    Implemented attributes for records, added new bytecode files to test

--- src/main/java/org/bytecodeparser/attribute/MethodParameters.java
+++ src/main/java/org/bytecodeparser/attribute/MethodParameters.java
@@ -1,7 +1,39 @@
 package org.bytecodeparser.attribute;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+
+import static org.bytecodeparser.attribute.MethodParameters.Parameter.readNParameters;
+
 public class MethodParameters extends AttributeInfo {
-    protected MethodParameters(short attributeNameIndex, int attributeLength) {
+
+    private final byte parametersCount;
+    private final Parameter[] parameters;
+
+    public MethodParameters(short attributeNameIndex, int attributeLength, DataInputStream dataInputStream) throws IOException {
         super(attributeNameIndex, attributeLength);
+        this.parametersCount = dataInputStream.readByte();
+        this.parameters = readNParameters(dataInputStream, this.parametersCount);
+    }
+
+    static class Parameter {
+
+        private final short nameIndex;
+        private final short accessFlags;
+
+        public static Parameter[] readNParameters(DataInputStream dataInputStream, int size)
+                throws IOException {
+            Parameter[] parameters = new Parameter[size];
+            for (int i = 0; i < size; i++) {
+                parameters[i] = new Parameter(dataInputStream);
+            }
+            return parameters;
+        }
+
+        public Parameter(DataInputStream dataInputStream) throws IOException {
+            this.nameIndex = dataInputStream.readShort();
+            this.accessFlags = dataInputStream.readShort();
+        }
+
     }
 }
--- src/main/java/org/bytecodeparser/attribute/Record.java
+++ src/main/java/org/bytecodeparser/attribute/Record.java
@@ -1,7 +1,47 @@
 package org.bytecodeparser.attribute;
 
+import org.bytecodeparser.annotation.ConsumeConstantPool;
+import org.bytecodeparser.constant.ConstantType;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+import static org.bytecodeparser.attribute.AttributeInfoReader.read;
+import static org.bytecodeparser.attribute.Record.RecordComponentInfo.readNComponents;
+
+@ConsumeConstantPool
 public class Record extends AttributeInfo {
-    protected Record(short attributeNameIndex, int attributeLength) {
+
+    private final short componentsCount;
+    private final RecordComponentInfo[] components;
+
+    public Record(short attributeNameIndex, int attributeLength, DataInputStream dataInputStream, ConstantType[] constantPool) throws IOException {
         super(attributeNameIndex, attributeLength);
+        this.componentsCount = dataInputStream.readShort();
+        this.components = readNComponents(dataInputStream, this.componentsCount, constantPool);
+    }
+
+    static class RecordComponentInfo {
+
+        private final short nameIndex;
+        private final short descriptorIndex;
+        private final short attributesCount;
+        private final AttributeInfo[] attributes;
+
+        public static RecordComponentInfo[] readNComponents(DataInputStream dataInputStream, int size, ConstantType[] constantPool)
+                throws IOException {
+            RecordComponentInfo[] parameters = new RecordComponentInfo[size];
+            for (int i = 0; i < size; i++) {
+                parameters[i] = new RecordComponentInfo(dataInputStream, constantPool);
+            }
+            return parameters;
+        }
+
+        public RecordComponentInfo(DataInputStream dataInputStream, ConstantType[] constantPool) throws IOException {
+            this.nameIndex = dataInputStream.readShort();
+            this.descriptorIndex = dataInputStream.readShort();
+            this.attributesCount = dataInputStream.readShort();
+            this.attributes = read(dataInputStream, attributesCount, constantPool);
+        }
     }
 }
